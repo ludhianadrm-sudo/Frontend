3bffe0b5f7a4cde36bbf0d0c41064cb18e429166
PBNICLDH-DIV2-AMAN <ludhianadrm@gmail.com>
2026-07-17 10:04:11 +0530

    Entry point for website updated

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -2,13 +2,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/header";
 import HomeScreen from "./(screens)/Home/page";
-import Entry from "./(screens)/Create/page";
+// import Entry from "./(screens)/Create/page";
 import View from "./(screens)/View/page";
 export default function Home() {
   return (
     <>
-    {/* <HomeScreen /> */}
-      <Entry />
+    <HomeScreen />
+      {/* <Entry /> */}
     </>
   );
 }
